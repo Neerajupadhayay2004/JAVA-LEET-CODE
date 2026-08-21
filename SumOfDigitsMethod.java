1c8d119858113f7c964e89397fe0b92256f2d9d4
Neeraj upadhayay <nupadhayay219@gmail.com>
2026-08-22 00:31:56 +0530

    Add sum of digits method program

---
+++ SumOfDigitsMethod.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOfDigitsMethod {
+    static int sumOfDigits(int n) {
+        n = Math.abs(n);
+        int sum = 0;
+
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Sum of digits = " + sumOfDigits(n));
+        sc.close();
+    }
+}
